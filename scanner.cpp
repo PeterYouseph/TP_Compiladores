7ad900475596a4e146f5804534fedccfed76876f
peteryouseph <peteryouseph@gmail.com>
2024-09-21 15:57:42 -0400

    feat: inclui verificação de comentários e divisão
    realiza também pequenas correções de nomes de lexemas (e.g.: INTERGER -> INTEGER)

--- scanner.cpp
+++ scanner.cpp
@@ -50,195 +50,268 @@ Token *Scanner::nextToken()
         switch (state)
         {
         case 0:
-            if (input[pos] == '\0')
+            if (input[pos] == '\0') // Verifica se chegou ao final do arquivo, retornando o token de END_OF_FILE
             {
                 tok = new Token(END_OF_FILE);
                 return tok;
             }
-            else if (isalpha(input[pos])){
+            else if (isalpha(input[pos])) // Verifica se o caractere é uma letra
+            {
                 lexeme = std::string(1, input[pos]);
                 state = 1;
             }
-            else if (isdigit(input[pos]))
+            else if (isdigit(input[pos])) // Verifica se o caractere é um dígito
                 state = 2;
-            else if (input[pos] == '\'') //verificar se ta certo
+            else if (input[pos] == '\'') // Verifica se o caractere é uma aspas simples
                 state = 3;
-            else if (input[pos] == '+'){
+            else if (input[pos] == '+') // Verifica se o caractere é um operador de soma
+            {
                 tok = new Token(OP, PLUS);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == '-'){
+            else if (input[pos] == '-') // Verifica se o caractere é um operador de subtração
+            {
                 tok = new Token(OP, MINUS);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == '*'){
+            else if (input[pos] == '*') // Verifica se o caractere é um operador de multiplicação
+            {
                 tok = new Token(OP, MULT);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == '=')
+            else if (input[pos] == '=') // Verifica se o caractere é um operador de atribuição
                 state = 5;
-            else if (input[pos] == '<')
+            else if (input[pos] == '<') // Verifica se o caractere é um operador de menor que (LT)
                 state = 6;
-            else if (input[pos] == '>')
+            else if (input[pos] == '>') // Verifica se o caractere é um operador de maior que (GT)
                 state = 7;
-            else if (input[pos] == '!')
+            else if (input[pos] == '!') // Verifica se o caractere é um operador de negação
                 state = 8;
-            else if (input[pos] == '&')
+            else if (input[pos] == '&') // Verifica se o caractere é um operador de AND
                 state = 9;
-            else if (input[pos] == '|')
+            else if (input[pos] == '|') // Verifica se o caractere é um operador de OR
                 state = 10;
-            else if (input[pos] == '('){
+            else if (input[pos] == '(') // Verifica se o caractere é um parêntese esquerdo
+            {
                 tok = new Token(SEP, PE);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == ')'){
+            else if (input[pos] == ')') // Verifica se o caractere é um parêntese direito
+            {
                 tok = new Token(SEP, PD);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == '{'){
+            else if (input[pos] == '{') // Verifica se o caractere é uma chave esquerda
+            {
                 tok = new Token(SEP, BE);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == '}'){
+            else if (input[pos] == '}') // Verifica se o caractere é uma chave direita
+            {
                 tok = new Token(SEP, BD);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == '['){
+            else if (input[pos] == '[') // Verifica se o caractere é um colchete esquerdo
+            {
                 tok = new Token(SEP, CE);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == ']'){
+            else if (input[pos] == ']') // Verifica se o caractere é um colchete direito
+            {
                 tok = new Token(SEP, CD);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == ','){
+            else if (input[pos] == ',') // Verifica se o caractere é uma vírgula
+            {
                 tok = new Token(SEP, VIRGULA);
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
-            else if (input[pos] == ';'){
+            else if (input[pos] == ';') // Verifica se o caractere é um ponto e vírgula
+            {
                 tok = new Token(SEP, PONTO_VIGULA);
+                pos++;  // Avança para o próximo caractere
+                return tok;
+            }
+            else if (input[pos] == '/' && input[pos + 1] == '/') // Verifica se o caractere é uma barra dupla (comentário de linha)
+            {
+                while (input[pos] != '\n')
+                    pos++;
+            }
+            else if (input[pos] == '/' && input[pos + 1] == '*') // Verifica se o caractere é uma barra asterisco (comentário em bloco)
+            {
+                pos += 2; // Avança após '/*'
+                while (!(input[pos] == '*' && input[pos + 1] == '/'))
+                {
+                    if (input[pos] == '\n')
+                        line++; // Conta novas linhas
+                    pos++;
+                }
+                pos += 2; // Avança após '*/'
+            }
+            else if (input[pos] == '/') // Verifica se o caractere é uma barra ( OP de divisão)
+            {
+                tok = new Token(OP, DIV);
+                pos++;  // Avança para o próximo caractere após '/'
                 return tok;
             }
             else if (isblank(input[pos]))
                 state = 11;
             else
-                //lexicalError();
-            // TO BE CONTINUED
-
-            pos++;
+                pos++; // Avança para o próximo caractere se for inválido no estado atual
             break;
 
         case 1:
-            if (!(isalnum(input[pos+1]))){
+            if (!(isalnum(input[pos + 1])) && input[pos + 1] != '_') // Verifica se o caractere não é alfanumérico ou um underline
+            {
                 l = std::string(1, input[pos]);
                 lexeme = lexeme + l;
                 tok = new Token(ID, lexeme);
+                pos++;  // Avança para o próximo caractere após processar o token
                 return tok;
             }
-            else{
+            else // Se o caractere for alfanumérico ou underline, ele é adicionado ao lexema
+            {
                 l = std::string(1, input[pos]);
                 lexeme = lexeme + l;
+                pos++;  // Continua processando
             }
-            pos++;
             break;
 
         case 2:
-            if (!(isdigit(input[pos+1]))){
-                tok = new Token(INTERGER);
+            if (!(isdigit(input[pos + 1]))) // Verifica se o caractere não é um dígito
+            {
+                tok = new Token(INTEGER);
+                pos++;  // Avança para o próximo caractere após processar o token
                 return tok;
             }
-            pos++;
+            pos++;  // Continua processando
             break;
-        
+
         case 3:
-            if (!(isprint(input[pos])) || input[pos] == '\\' || input[pos] == '\''){
-                msg = "Character recebido não é ASCII imprimível ou é \\ ou \'.";
+            if (!(isprint(input[pos])) || input[pos] == '\\' || input[pos] == '\'')
+            {
+                msg = "Caractere recebido não é ASCII imprimível ou é \\ ou \'.";
                 lexicalError(msg);
             }
             state = 4;
             pos++;
             break;
-        
+
         case 4:
-            if (input[pos] == '\''){
+            if (input[pos] == '\'')
+            {
                 tok = new Token(CHAR);
+                pos++;  // Avança para o próximo caractere após processar o token
                 return tok;
             }
-            else{
-                msg = "Esperado aspas simples '. Recebeu: " + std::string(1, input[pos]);
+            else
+            {
+                msg = "Esperado aspas simples '. Recebido: " + std::string(1, input[pos]);
                 lexicalError(msg);
             }
-        
+            break;
+
         case 5:
-            if (input[pos] == '='){
+            if (input[pos + 1] == '=')
+            {
                 tok = new Token(RELOP, EQUAL);
+                pos += 2; // Avança após '=='
                 return tok;
             }
-            else{
+            else
+            {
                 tok = new Token(RELOP, EQ_COM);
-                // pos--; VERIFICAR SE PRECISA
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
 
         case 6:
-            if (input[pos] == '='){
+            if (input[pos + 1] == '=')
+            {
                 tok = new Token(RELOP, LE);
+                pos += 2; // Avança após '<='
                 return tok;
             }
-            else{
+            else
+            {
                 tok = new Token(RELOP, LT);
-                // pos--; VERIFICAR SE PRECISA
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
 
         case 7:
-            if (input[pos] == '='){
+            if (input[pos + 1] == '=')
+            {
                 tok = new Token(RELOP, GE);
+                pos += 2; // Avança após '>='
                 return tok;
             }
-            else{
+            else
+            {
                 tok = new Token(RELOP, GT);
-                // pos--; VERIFICAR SE PRECISA
+                pos++;  // Avança para o próximo caractere
                 return tok;
             }
 
         case 8:
-            if (input[pos] == '='){
+            if (input[pos + 1] == '=')
+            {
                 tok = new Token(RELOP, DIF);
+                pos += 2; // Avança após '!='
                 return tok;
             }
-            else{
-                tok = new Token(RELOP, NOT);
-                // pos--; VERIFICAR SE PRECISA
-                return tok;
-            }
-        
-        case 9:
-            if (input[pos] == '&'){
-                tok = new Token(RELOP, AND);
-                return tok;
-            }
-            else{
-                msg = "Caracter esperado &. Recebido: " + std::string(1, input[pos]);
+            else
+            {
+                msg = "Esperado =. Recebido: " + std::string(1, input[pos + 1]);
                 lexicalError(msg);
             }
-        
-        case 10:
-            if (input[pos] == '|'){
-                tok = new Token(RELOP, OR);
-                return tok;
-            }
-            else{
-                msg = "Caracter esperado |. Recebido: " + std::string(1, input[pos]);
-                lexicalError(msg);
-            }
-
-        case 11:
-            if (!isspace(input[pos]))
-                state = 0;
             break;
 
-        //     if (input[pos] == '=')
+        case 9:
+            if (input[pos + 1] == '&')
+            {
+                tok = new Token(OP, AND);
+                pos += 2; // Avança após '&&'
+                return tok;
+            }
+            else
+            {
+                msg = "Esperado &. Recebido: " + std::string(1, input[pos + 1]);
+                lexicalError(msg);
+            }
+            break;
+
+        case 10:
+            if (input[pos + 1] == '|')
+            {
+                tok = new Token(OP, OR);
+                pos += 2; // Avança após '||'
+                return tok;
+            }
+            else
+            {
+                msg = "Esperado |. Recebido: " + std::string(1, input[pos + 1]);
+                lexicalError(msg);
+            }
+            break;
+
+        case 11:
+            if (input[pos] == '\n') // Verifica se é uma nova linha
+                line++;
+            pos++;
+            state = 0; // Volta para o estado inicial
+            break;
+                //     if (input[pos] == '=')
         //         state = 2;
         //     else if (input[pos] == '>')
         //         state = 3;
@@ -395,7 +468,7 @@ Token *Scanner::nextToken()
         //     //lexicalError();
         }
     }
-} // Fim nextToken
+}// Fim nextToken
 
 void Scanner::lexicalError(string msg)
 {
